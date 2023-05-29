d806e7b3de90488100bacabc3c1f23954d02adfb
AndreySchmidt <ringnull@gmail.com>
2023-05-29 10:44:52 +0300

    npm i argon2 + user create

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail()
     email: string;
+
+    @MinLength(6, {message: 'Password must be 6 or more symbols'})
     password: string;
 }
